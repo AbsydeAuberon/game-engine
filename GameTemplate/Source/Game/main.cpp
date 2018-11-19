9a5a9b244f530605dacb172bccda4f8f5f39910e
Álvaro Tomás <auberontermeraz@gmail.com>
2018-11-19 16:33:14 +0100

    Test
    1

--- GameTemplate/Source/Game/main.cpp
+++ GameTemplate/Source/Game/main.cpp
@@ -1,5 +1,6 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2015)
 and may not be redistributed without written permission.*/
+//YA VES
 
 //Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
